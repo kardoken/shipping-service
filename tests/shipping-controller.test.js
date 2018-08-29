dfdc75a917edd0c8d4073206a6c87ad03d7e3938
KARUNS12 <karuns12@uk.ibm.com>
2018-08-29 11:01:57 +0100

    creating test cases

--- tests/shipping-controller.test.js
+++ tests/shipping-controller.test.js
@@ -1,4 +1,13 @@
-describe('Shipping Controller', function() {
-    // Test cases will go here
-    
+var chai = require('chai')
+var expect = chai.expect
+var ShippingController = require('../src/controllers/shipping-controller')
+
+describe('Shipping controller', function () {
+  var shippingCtrl = new ShippingController()
+
+  it('Should calculate correct shipping ', async function () {
+    let shipping = await shippingCtrl.getItemShipping({ id: 1, type: 'standard' })
+    expect(shipping).to.equal(0.5)
+  })
+
 })
